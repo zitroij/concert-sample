f87468fde16d820da3921dfb806b4f0617ad8b17
Jose Ortiz <jortiz@us.ibm.com>
2024-06-04 18:08:37 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+
+scriptdir=`dirname $0`
+cd ${scriptdir}
+scriptdir=`pwd`
+sourcecodedir=$(builtin cd $scriptdir/..; pwd)
+
 if test -z "$IMAGE"; then
    IMAGE="concert-sample"
 fi
@@ -10,7 +16,7 @@ fi
 
 
 # shellcheck disable=SC2086
-docker build -f Dockerfile -t ${IMAGE}:${TAG}
+docker build -f $sourcecodedir/Dockerfile -t ${IMAGE}:${TAG} 
 
 
 DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}" | awk -F@ '{print $2}')"

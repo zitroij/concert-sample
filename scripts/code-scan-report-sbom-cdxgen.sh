9ad5b7a9a04b82d680228bbed31b0eacfc62d274
Jose Ortiz <jortiz@us.ibm.com>
2024-06-05 16:24:56 -0400

    Update code-scan-report-sbom-cdxgen.sh

--- scripts/code-scan-report-sbom-cdxgen.sh
+++ scripts/code-scan-report-sbom-cdxgen.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+
 scriptdir=`dirname $0`
 cd ${scriptdir}
 scriptdir=`pwd`
+sourcecodedir=$(builtin cd $scriptdir/..; pwd)
 
+VARIABLES_FILE=${sourcecodedir}/concert_data/demo_build_envs.variables
 
-OUTPUT_FILENAME="code-scancyclonedx-sbom.json"
+source ${VARIABLES_FILE}
+
+OUTPUT_FILENAME_SUFFIX="${BUILD_NUMBER}-build-vars"
 sourcecodedir=$(builtin cd $scriptdir/..; pwd)
 
 echo "${SRC_PATH}" > /dev/null
-docker run -it --rm -u $(id -u):$(id -g) -v $sourcecodedir:/data localhost/ibm-concert-toolkit:v1 bash -c '/app/code-scan-sbom-cdxgen.sh --src /data --out /data/concert_data'
+docker run -it --rm -u $(id -u):$(id -g) -v $sourcecodedir:/data localhost/ibm-concert-toolkit:v1 bash -c '/app/code-scan-sbom-cdxgen.sh --src /data --output-file ${OUTPUT_FILENAME_SUFFIX}'
 echo > /dev/null
 

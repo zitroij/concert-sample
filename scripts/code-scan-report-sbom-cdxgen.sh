7af8eb83d88ae060a40d76d5b9197ba01fe6d1b8
Jose Ortiz <jortiz@us.ibm.com>
2024-06-05 08:22:01 -0400

    Update code-scan-report-sbom-cdxgen.sh

--- scripts/code-scan-report-sbom-cdxgen.sh
+++ scripts/code-scan-report-sbom-cdxgen.sh
@@ -9,6 +9,6 @@ OUTPUT_FILENAME="code-scancyclonedx-sbom.json"
 sourcecodedir=$(builtin cd $scriptdir/..; pwd)
 
 echo "${SRC_PATH}" > /dev/null
-docker run -it --rm -v $sourcecodedir:/input localhost/ibm-concert-toolkit:v1 bash -c '/app/code-scan-sbom-cdxgen.sh --src /input --out /input/concert_data/code_scan_sbom.json'
+docker run -it --rm -u $(id -u):$(id -g) -v $sourcecodedir:/input localhost/ibm-concert-toolkit:v1 bash -c '/app/code-scan-sbom-cdxgen.sh --src /input --out /input/concert_data/code_scan_sbom.json'
 echo > /dev/null
 

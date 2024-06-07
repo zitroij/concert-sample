51ddfa7ea3322494988a6a64e628efcd8ef2b488
Jose Ortiz <jortiz@us.ibm.com>
2024-06-07 18:43:54 -0400

    emulating concert

--- scripts/helpers/concert-release-inventory.sh
+++ scripts/helpers/concert-release-inventory.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-
-. ../concert_data/demo_release_envs.variables
-
-RELEASE_FILENAME=../concert-data/release-config.yaml
-
-cat > ${RELEASE_FILENAME} << EOD
-concert:
-    release:
-      app_name: ${APP_NAME}
-      number: ${BUILD_NUMBER}
-      version: ${APP_VERSION}
-EOD
+### WORK IN PROGESS

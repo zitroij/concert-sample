109f9b2178e7e2ef422a083e6193aebabd14014a
Jose Ortiz <jortiz@us.ibm.com>
2024-06-07 18:49:08 -0400

    Update concert_upload_data.sh

--- scripts/helpers/concert_upload_data.sh
+++ scripts/helpers/concert_upload_data.sh
@@ -12,9 +12,4 @@ SRC_PATH=${sourcecodedir}/src
 
 source ${VARIABLES_FILE}
 
-OUTPUT_FILENAME="$REPO_NAME-$BUILD_NUMBER-cyclonedx-sbom.json"
-sourcecodedir=$(builtin cd $scriptdir/../..; pwd)
-
-
-CODE_SCAN_COMMAND="code-scan-sbom-cdxgen.sh --src /concert-sample-src --output-file ${OUTPUT_FILENAME}"
-docker run -it --rm -u $(id -u):$(id -g) -v ${SRC_PATH}:/concert-sample-src -v ${OUTPURDIR}:/toolkit-data localhost/ibm-concert-toolkit:v1 bash -c "${CODE_SCAN_COMMAND}"
+### WORK IN PROGESS

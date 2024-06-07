85deb6e04862e34acd92bcd790ca4c7f0b35f3c7
Jose Ortiz <jortiz@us.ibm.com>
2024-06-07 19:09:08 -0400

    Update application_definition.sh

--- scripts/application_definition.sh
+++ scripts/application_definition.sh
@@ -1,10 +1,9 @@
 ####
 
-
 scriptdir=`dirname $0`
 cd ${scriptdir}
 scriptdir=`pwd`
-sourcecodedir=$(builtin cd $scriptdir/../..; pwd)
+sourcecodedir=$(builtin cd $scriptdir/..; pwd)
 
 VARIABLES_FILE=${sourcecodedir}/concert_data/demo_build_envs.variables
 

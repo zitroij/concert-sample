613cb414ca117780db0f496d5a8418988a3faffb
Jose Ortiz <jortiz@us.ibm.com>
2024-06-10 08:01:56 -0400

    enabling updload-files

---
+++ scripts/helpers/updateToConcert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+scriptdir=`dirname $0`
+cd ${scriptdir}
+scriptdir=`pwd`
+sourcecodedir=$(builtin cd $scriptdir/../..; pwd)
+
+VARIABLES_FILE=${sourcecodedir}/concert_data/demo_build_envs.variables
+
+OUTPURDIR=${sourcecodedir}/concert_data
+SRC_PATH=${sourcecodedir}/src
+
+source ${VARIABLES_FILE}
+
+
+
+CODE_SCAN_COMMAND="upload-files.py"
+docker run -it --rm -u $(id -u):$(id -g) -v ${OUTPURDIR}:/toolkit-data localhost/ibm-concert-toolkit:v1 bash -c "${CODE_SCAN_COMMAND}"

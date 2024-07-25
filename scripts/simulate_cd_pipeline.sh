f55499362070fbdc7fa60bd9eed6dbbac00beb04
Jose Ortiz <jortiz@us.ibm.com>
2024-07-25 11:23:36 -0400

    working on cd flow

---
+++ scripts/simulate_cd_pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+####
+
+scriptdir=`dirname $0`
+cd ${scriptdir}
+scriptdir=`pwd`
+sourcecodedir=$(builtin cd $scriptdir/..; pwd)
+
+VARIABLES_FILE=${sourcecodedir}/concert_data/demo_build_envs.variables
+
+source ${VARIABLES_FILE}
+
+export OUTPUTDIR=${sourcecodedir}/concert_data
+
+
+echo "#####"
+echo "# gen concert build inventory #"
+echo "####"
+
+./concert-utils/helpers/gen-deploy-inventory.sh
+
+
+###
+# upload build file
+###
+#echo "generating config file inventory json ${OUTPUTDIR}/${outfile_name} "
+
+envsubst < ${OUTPUTDIR}/simulating_cd_config.yaml.template > ${OUTPUTDIR}/config.yaml
+
+#./concert-utils/helpers/concert_upload_data.sh

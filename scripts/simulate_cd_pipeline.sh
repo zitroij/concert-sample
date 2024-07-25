0ecdc7a3ffbf1644a83845d54d8030ec8db105ae
Jose Ortiz <jortiz@us.ibm.com>
2024-07-25 14:12:48 -0400

    Update simulate_cd_pipeline.sh

--- scripts/simulate_cd_pipeline.sh
+++ scripts/simulate_cd_pipeline.sh
@@ -12,20 +12,17 @@ source ${VARIABLES_FILE}
 
 export OUTPUTDIR=${sourcecodedir}/concert_data
 
-
 echo "#####"
 echo "# gen concert deploy inventory #"
 echo "####"
+
 # define file name as input
-
 ./concert-utils/helpers/gen-deploy-inventory.sh
-
 
 ###
 # upload build file
 ###
 #echo "generating config file inventory json ${OUTPUTDIR}/${outfile_name} "
-
 envsubst < ${OUTPUTDIR}/simulating_cd_config.yaml.template > ${OUTPUTDIR}/config.yaml
 
-#./concert-utils/helpers/concert_upload_data.sh
+./concert-utils/helpers/concert_upload_data.sh

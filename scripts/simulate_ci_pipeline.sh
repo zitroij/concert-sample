b93b975d1fa968e48e479346387cca1b6c87aba4
Jose Ortiz <jortiz@us.ibm.com>
2024-07-26 12:38:03 -0400

    Update simulate_ci_pipeline.sh

--- scripts/simulate_ci_pipeline.sh
+++ scripts/simulate_ci_pipeline.sh
@@ -15,6 +15,7 @@ export SRC_PATH=${sourcecodedir}/src
 
 echo "#####"
 echo "# source scanning stage #"
+echo "# ./concert-utils/helpers/code-scan-stage-gen-cyclondx-sbom.sh --outputfile ${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json "
 echo "#####"
 ./concert-utils/helpers/code-scan-stage-gen-cyclondx-sbom.sh --outputfile "${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json"
 
@@ -24,22 +25,27 @@ echo "#####"
 
 ./build.sh
 
+export CYCLONEDX_FILENAME="${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json"
+
 echo "#####"
 echo "# image scanning stage #"
+echp "# ./concert-utils/helpers/image-scan-stage-gen-cyclondx-sbom.sh --outputfile ${CYCLONEDX_FILENAME}"
 echo "#####"
-export CYCLONEDX_FILENAME="${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json"
 ./concert-utils/helpers/image-scan-stage-gen-cyclondx-sbom.sh --outputfile ${CYCLONEDX_FILENAME}
+
+
+export BUILD_FILE_NAME="${COMPONENT_NAME}-build-inventory-${BUILD_NUMBER}.json"
 
 echo "#####"
 echo "# gen concert build inventory #"
+echo "# ./concert-utils/helpers/gen-build-inventory.sh --outputfile ${BUILD_FILE_NAME}"
 echo "#####"
-export BUILD_FILE_NAME="${COMPONENT_NAME}-build-inventory-${BUILD_NUMBER}.json"
 ./concert-utils/helpers/gen-build-inventory.sh --outputfile ${BUILD_FILE_NAME}
 
 
 echo "#####"
 echo "# send to concert stage #"
+echo "#./concert-utils/helpers/concert_upload_data.sh"
 echo "#####"
 envsubst < ${OUTPUTDIR}/simulating_ci_config.yaml.template > ${OUTPUTDIR}/config.yaml
-
 ./concert-utils/helpers/concert_upload_data.sh

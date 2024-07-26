c0e8f54533fec379e408e833c09bfab1ec1cd129
Jose Ortiz <jortiz@us.ibm.com>
2024-07-26 17:38:18 -0400

    Update application_definition.sh

--- scripts/application_definition.sh
+++ scripts/application_definition.sh
@@ -10,22 +10,18 @@ VARIABLES_FILE=${sourcecodedir}/concert_data/demo_build_envs.variables
 
 source ${VARIABLES_FILE}
 
-#####
-# gen concert app inventory
-####
 export APP_FILE_NAME="${APP_NAME}-${APP_VERSION}-application.json"
+
+echo "#####"
+echo "# gen concert app inventory"
+echo "# ./concert-utils/helpers/gen-concert-application.sh --outfile ${APP_FILE_NAME}"
+echo "####"
 
 ./concert-utils/helpers/gen-concert-application.sh --outfile ${APP_FILE_NAME}
 
-cd ${scriptdir}
-
-#####
-# send to concert stage
-####
-###
-# upload build file
-###
-#echo "generating config file inventory json ${OUTPUTDIR}/${outfile_name} "
+echo "#####"
+echo "# send to concert stage"
+echo "#####"
 
 envsubst < ${OUTPUTDIR}/application_def_load_config.yaml.template > ${OUTPUTDIR}/config.yaml
 ./concert-utils/helpers/concert_upload_data.sh

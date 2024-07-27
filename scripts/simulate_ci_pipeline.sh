55bfd9f0460b3df5f97f319094f819f3c75c192f
Jose Ortiz <jortiz@us.ibm.com>
2024-07-27 10:44:24 -0400

    Update simulate_ci_pipeline.sh

--- scripts/simulate_ci_pipeline.sh
+++ scripts/simulate_ci_pipeline.sh
@@ -25,23 +25,25 @@ echo "#####"
 
 ./build.sh
 
-export CYCLONEDX_FILENAME="${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json"
+export CYCLONEDX_FILENAME=${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json
 
 echo "#####"
-echo "# image scanning stage #"
+echo "# image scanning stage "
 echo "# ./concert-utils/helpers/image-scan-stage-gen-cyclondx-sbom.sh --outputfile ${CYCLONEDX_FILENAME}"
 echo "#####"
 #./concert-utils/helpers/image-scan-stage-gen-cyclondx-sbom.sh --outputfile ${CYCLONEDX_FILENAME}
 
-export BUILD_FILENAME="${COMPONENT_NAME}-build-inventory-${BUILD_NUMBER}.json"
 
-CONCERT_DEF_CONFIG_FILE="build-${COMPONENT_NAME}-${BUILD_NUMBER}-config.yaml"
+echo "#####"
+echo "# gen concert build inventory (build sbom) "
+
+export BUILD_FILENAME=${COMPONENT_NAME}-build-inventory-${BUILD_NUMBER}.json
+
+CONCERT_DEF_CONFIG_FILE=build-${COMPONENT_NAME}-${BUILD_NUMBER}-config.yaml
 
 echo "envsubst < ${scriptdir}/${TEMPLATE_PATH}/build-sbom-values.yaml.template > ${OUTPUTDIR}/${CONCERT_DEF_CONFIG_FILE}"
 envsubst < ${scriptdir}/${TEMPLATE_PATH}/build-sbom-values.yaml.template > ${OUTPUTDIR}/${CONCERT_DEF_CONFIG_FILE}
 
-echo "#####"
-echo "# gen concert build inventory (build sbom) "
 echo "# ./concert-utils/helpers/gen-build-inventory.sh --outputdir ${OUTPUTDIR} --configfile ${CONCERT_DEF_CONFIG_FILE}"
 echo "#####"
 ./concert-utils/helpers/gen-build-inventory.sh --outputdir ${OUTPUTDIR} --configfile ${CONCERT_DEF_CONFIG_FILE}

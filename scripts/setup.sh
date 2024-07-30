3a4f727ca4acf3d65be4b606785be595a9a05cbe
Jose Ortiz <jortiz@us.ibm.com>
2024-07-30 13:05:09 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,9 +17,9 @@ source ${VARIABLES_FILE}
 git clone ${CONCERT_TOOLKIT_UTILS_REPO}
 
 if which docker >/dev/null; then
-    dockerexe = docker 
+    dockerexe=docker 
 elif which podman >/dev/null; then
-    dockerexe podman
+    dockerexe=podman
 else
     echo "docker or podman are not installed need a container runtime environment"
     exit -1
